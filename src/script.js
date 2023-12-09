80ddccd0b93c507be6a6c7b79099490df18c5bf8
vasiliv <32122468+vasiliv@users.noreply.github.com>
2023-12-09 10:38:48 +0400

    first commit

---
+++ src/script.js
@@ -0,0 +1,17 @@
+// Get the element by its ID
+const myElement = document.getElementById('myElement');
+const colorWhite = document.getElementsByClassName("bg-color-white");
+const colorGreen = document.getElementsByClassName("bg-color-green");
+
+
+// Check a condition (e.g., a variable, user interaction, etc.)
+const condition = true; // Change this to your actual condition
+
+// Change the style based on the condition
+if (condition) {
+    myElement.style.color = 'red';
+    // Add more style changes as needed
+} else {
+    myElement.style.color = 'blue';
+    // Add more style changes as needed
+}
